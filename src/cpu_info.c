f4fce17e92b536b77e64218c4ad857efe768eed9
Xlebovoz <lebed3108alex@gmail.com>
2025-11-29 20:20:11 +0300

    1.1

--- src/cpu_info.c
+++ src/cpu_info.c
@@ -1,36 +1,121 @@
 #include "../include/monitor.h"
+#include <pdh.h>
+#include <pdhmsg.h>
+#include <conio.h>
 
-void show_cpu_info() {
-    printf("\n=== ИНФОРМАЦИЯ О CPU ===\n");
+#pragma comment(lib, "pdh.lib")
+
+void print_bar(int percentage, int length) {
+    if (percentage < 0) percentage = 0;
+    if (percentage > 100) percentage = 100;
     
+    int bars = (percentage * length) / 100;
+    printf("[");
+    for(int i = 0; i < length; i++) {
+        if(i < bars) printf("█");
+        else printf("░");
+    }
+    printf("] %d%%", percentage);
+}
+
+// Инициализация PDH один раз при старте
+int init_pdh_counters(PDH_HQUERY* query, PDH_HCOUNTER* counter) {
+    if (PdhOpenQuery(NULL, 0, query) != ERROR_SUCCESS) return 0;
+    if (PdhAddEnglishCounter(*query, "\\Processor(_Total)\\% Processor Time", 0, counter) != ERROR_SUCCESS) {
+        PdhCloseQuery(*query);
+        return 0;
+    }
+    PdhCollectQueryData(*query);
+    Sleep(1000); // Первая калибровка
+    return 1;
+}
+
+// Быстрое получение загрузки CPU через PDH
+int get_real_cpu_load_pdh(PDH_HQUERY query, PDH_HCOUNTER counter) {
+    PdhCollectQueryData(query);
+    
+    PDH_FMT_COUNTERVALUE counterVal;
+    if (PdhGetFormattedCounterValue(counter, PDH_FMT_DOUBLE, NULL, &counterVal) == ERROR_SUCCESS) {
+        return (int)counterVal.doubleValue;
+    }
+    return -1;
+}
+
+void show_cpu_info_real_time() {
+    printf("=== REALTIME CPU MONITOR (PDH API) ===\n");
+    printf("Нажми ESC для выхода\n\n");
+    
+    // Инициализируем PDH
+    PDH_HQUERY cpuQuery;
+    PDH_HCOUNTER cpuCounter;
+    
+    if (!init_pdh_counters(&cpuQuery, &cpuCounter)) {
+        printf("Ошибка инициализации PDH!\n");
+        return;
+    }
+    
+    // Информация о процессоре
     SYSTEM_INFO sysInfo;
     GetSystemInfo(&sysInfo);
     
-    printf("Архитектура: ");
-    switch(sysInfo.wProcessorArchitecture) {
-        case PROCESSOR_ARCHITECTURE_AMD64:
-            printf("x64 (64-битная)\n"); break;
-        case PROCESSOR_ARCHITECTURE_INTEL:
-            printf("x86 (32-битная)\n"); break;
-        case PROCESSOR_ARCHITECTURE_ARM:
-            printf("ARM\n"); break;
-        default:
-            printf("Неизвестно\n");
-    }
-    
-    printf("Тип процессора: %d\n", sysInfo.dwProcessorType);
-    printf("Количество ядер: %d\n", sysInfo.dwNumberOfProcessors);
-    printf("Размер страницы: %d KB\n", sysInfo.dwPageSize / 1024);
-    
-    // Дополнительная информация о процессоре
     HKEY hKey;
-    DWORD bufferSize = 255;
-    char cpuName[255];
+    DWORD size = 255;
+    char cpuName[255] = "Неизвестный процессор";
     
-    if(RegOpenKeyEx(HKEY_LOCAL_MACHINE, "HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0", 0, KEY_READ, &hKey) == ERROR_SUCCESS) {
-        if(RegQueryValueEx(hKey, "ProcessorNameString", NULL, NULL, (LPBYTE)cpuName, &bufferSize) == ERROR_SUCCESS) {
-            printf("Модель: %s\n", cpuName);
-        }
+    if(RegOpenKeyEx(HKEY_LOCAL_MACHINE, 
+                   "HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0", 
+                   0, KEY_READ, &hKey) == ERROR_SUCCESS) {
+        RegQueryValueEx(hKey, "ProcessorNameString", NULL, NULL, (LPBYTE)cpuName, &size);
         RegCloseKey(hKey);
     }
+    
+    int frame = 0;
+    
+    while(1) {
+        if(_kbhit() && _getch() == 27) break;
+        
+        system("cls");
+        printf("=== REALTIME CPU MONITOR (кадр %d) ===\n", ++frame);
+        printf("Нажми ESC для выхода\n\n");
+        
+        printf("Процессор: %s\n", cpuName);
+        printf("Ядер: %d\n\n", sysInfo.dwNumberOfProcessors);
+        
+        // РЕАЛЬНАЯ загрузка через PDH (БЫСТРО!)
+        int real_load = get_real_cpu_load_pdh(cpuQuery, cpuCounter);
+        
+        if(real_load >= 0) {
+            printf("=== ТЕКУЩАЯ ЗАГРУЗКА ===\n");
+            print_bar(real_load, 30);
+            printf("\n\n");
+        }
+        
+        printf("=== ЗАГРУЗКА ПО ЯДРАМ ===\n");
+        
+        // Демо-данные для ядер на основе реальной загрузки
+        srand(GetTickCount() + frame);
+        for(int i = 0; i < sysInfo.dwNumberOfProcessors; i++) {
+            int load = real_load >= 0 ? 
+                      real_load + (rand() % 20 - 10) : // ±10% от реальной
+                      20 + (rand() % 60);
+            
+            if(load < 0) load = 0;
+            if(load > 100) load = 100;
+            
+            printf("Ядро %d: ", i);
+            print_bar(load, 20);
+            printf("\n");
+        }
+        
+        printf("\nОбновление: мгновенное\n");
+        printf("Время: %d сек\n", frame);
+        
+        Sleep(500); // Обновление 2 раза в секунду
+    }
+    
+    PdhCloseQuery(cpuQuery);
+}
+
+void show_cpu_info() {
+    show_cpu_info_real_time();
 }
